c948340623bbaa36de6a7e1063f4d0007abeecde
Sanjay Boddu <sboddu@ebi.ac.uk>
2019-05-17 15:08:26 +0100

    Make division list unique

--- resources/genome_store.py
+++ resources/genome_store.py
@@ -23,16 +23,21 @@ class GenomeStore(object):
             if genome_sub_key in genome and genome[genome_sub_key] == genome_sub_value:
                 return genome_key
 
+#    def update_genome_store(self, genome_key, genome_value):
+#        self.genome_store[genome_key] = genome_value
+
     def add_to_genome_store(self, genome):
 
         existing_genome_key = self.check_if_genome_exists('genome_id', genome.genome_id)
 
         if existing_genome_key is not None:
-            # TODO: Logic for updating existing genome
+            # TODO: Logic for updating existing genome. At the moment, we are updating only division.
 
             existing_genome = Genome(self.get_genome(existing_genome_key))
             existing_genome.create_genome_from_genome_store()
             existing_genome.division.extend(genome.division)
+
+            existing_genome.division = list(set(existing_genome.division))
 
             self.genome_store[existing_genome_key] = existing_genome.convert_to_dict()
 
