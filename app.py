395060d00e7831f6a295bd38053be150b39bb8e5
Sanjay Boddu <sboddu@ebi.ac.uk>
2019-05-29 15:34:57 +0100

    Use Flask blueprints

--- app.py
+++ app.py
@@ -1,48 +1,36 @@
-from flask import Flask, jsonify, make_response
-from flask_restful import Resource, Api
-import os, json
+from flask import Flask
+import os, json, sys
 from resources.genome_store import GenomeStore
 from resources.ensembl_indexer import Indexer
-from configs.app_config import get_config
+from configs.config import get_config
 
 
+app = Flask(__name__)
+app.config.update(get_config())
 
-application = Flask(__name__)
-application.config.update(get_config())
-api = Api(application)
-
-
-class Default(Resource):
-    def get(self, path=''):
-        # Return 404 response status code with error message if user queries any endpoint other than species
-        return make_response(jsonify({'error': 'No such endpoint'}), 404)
-
-
-class Species(Resource):
-    def get(self, species_query, division):
-        response_payload = {}
-
-        genome_keys = indexer.search(species_query.lower())
-
-        if genome_keys is not None:
-            response_payload = {genome_key: genome_store.get_genome(genome_key) for genome_key in genome_keys}
-
-        return make_response(jsonify({'match': response_payload}), 200)
-
-
-api.add_resource(Default, '/', '/<path:path>')
-api.add_resource(Species, '/species/<string:species_query>/<string:division>')
-
-
-
-if os.path.isfile(application.config['INDEX_FILE']):
-    print(application.config['INDEX_FILE'])
-    with open(application.config['INDEX_FILE'], 'r') as index_file:
+if os.path.isfile(app.config['INDEX_FILE']):
+    print(app.config['INDEX_FILE'])
+    with open(app.config['INDEX_FILE'], 'r') as index_file:
         indexes = json.load(index_file)
-        indexer = Indexer(indexes)
+        app.indexer = Indexer(indexes)
+else:
+    sys.exit('Problem opening Index file:{}'.format(app.config['INDEX_FILE']))
 
-if os.path.isfile(application.config['GENOME_STORE_FILE']):
-    print(application.config['GENOME_STORE_FILE'])
-    with open(application.config['GENOME_STORE_FILE'], "r") as genome_store_file:
+if os.path.isfile(app.config['GENOME_STORE_FILE']):
+    print(app.config['GENOME_STORE_FILE'])
+    with open(app.config['GENOME_STORE_FILE'], "r") as genome_store_file:
         genome_store_data = json.load(genome_store_file)
-        genome_store = GenomeStore(genome_store_data)
+        app.genome_store = GenomeStore(genome_store_data)
+else:
+    sys.exit('Problem opening Genome store file:{}'.format(app.config['GENOME_STORE_FILE']))
+
+
+with app.app_context():
+    from blueprints import genome_search
+    app.register_blueprint(genome_search.search_bp, url_prefix='/genome_search')
+
+
+print("Starting the server...")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8011)
---
+++ blueprints/genome_search.py
@@ -0,0 +1,32 @@
+from flask import Blueprint, jsonify, make_response
+from flask import current_app as app
+from flask_restful import Resource, Api
+
+
+search_bp = Blueprint('genome_search', __name__)
+api = Api(search_bp)
+
+
+print(app.indexer)
+
+class Default(Resource):
+    def get(self, path=''):
+        # Return 404 response status code with error message if user queries any endpoint other than species
+        return make_response(jsonify({'error': 'No such endpoint'}), 404)
+
+
+class Species(Resource):
+    def get(self, species_query, division):
+        response_payload = {}
+
+        genome_keys = app.indexer.search(species_query.lower())
+
+        if genome_keys is not None:
+            response_payload = {genome_key: app.genome_store.get_genome(genome_key) for genome_key in genome_keys}
+
+        return make_response(jsonify({'match': response_payload}), 200)
+
+
+
+api.add_resource(Default, '/', '/<path:path>')
+api.add_resource(Species, '/species/<string:species_query>/<string:division>')
