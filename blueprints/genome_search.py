01e24cddccca78da7e6975bf888be291cd185d4d
Sanjay Boddu <sboddu@ebi.ac.uk>
2019-06-01 14:50:19 +0100

    Code refactor

--- blueprints/genome_search.py
+++ blueprints/genome_search.py
@@ -1,15 +1,11 @@
 from flask import Blueprint, jsonify, make_response, abort, request
 from flask import current_app as app
 from flask_restful import Resource, Api, reqparse
-import re
 from resources.genome import Genome
 
 
 search_bp = Blueprint('genome_search', __name__)
 api = Api(search_bp)
-
-
-# print(app.indexes)
 
 
 class Search(Resource):
@@ -24,47 +20,42 @@ class Search(Resource):
         # Split user query to work on each word
         self.query_words = self.args.query.split()
 
-        # print(args)
+        grouped_by_match_position = {}
 
-        response_payload = {}
-        grouped_genomes = {}
-
-        genome_keys =self._get_genome_keys()
+        genome_keys = self._get_genome_keys()
 
         for genome_key in genome_keys:
             genome = self._get_genome(genome_key)
-            matched_locations = self._find_match_locations(genome)
+            matched_positions = self._locate_match_positions(genome)
+            grouped_by_match_position = self._group_by_match_position(grouped_by_match_position, matched_positions)
 
-            grouped_genomes = self._group_by_location(matched_locations, grouped_genomes)
+            # Todo: sort within groups
+            # sorted_results = self.sort_results_by(grouped_by_match_position, 'common_name')
 
-        print(grouped_genomes)
+        print(grouped_by_match_position)
 
-        response = self._prepare_response(grouped_genomes)
-
+        response = self._prepare_response(grouped_by_match_position)
 
         return make_response(jsonify({'match': response}), 200)
 
-
     def _get_genome_keys(self):
 
-
         # Get Genome keys for every word of use query
-        all_genome_keys = []
+        genome_keys = []
         for query_word in self.query_words:
             genome_keys_of_query_word = app.indexes.search(query_word)
             if genome_keys_of_query_word is None:
-                all_genome_keys.append(set())
+                genome_keys.append(set())
             else:
-                all_genome_keys.append(set(genome_keys_of_query_word))
+                genome_keys.append(set(genome_keys_of_query_word))
 
         # Create a set of Genome keys which are common to all the words in a user query
-        valid_genome_keys = set.intersection(*all_genome_keys)
+        valid_genome_keys = set.intersection(*genome_keys)
 
-        print(all_genome_keys)
-        print(valid_genome_keys)
+        # print(genome_keys)
+        # print(valid_genome_keys)
 
         return valid_genome_keys
-
 
     def _get_genome(self, genome_key):
 
@@ -73,83 +64,66 @@ class Search(Resource):
         # genome.create_genome_from_gs_format()
         # genome.sanitize()
 
+    def _locate_match_positions(self, genome):
 
-    def _find_match_locations(self, genome):
-
-        group_and_location_summary = {}
+        genome_with_matched_positions = {}
         # is there a match in common name? get the coordinates
         for genome_key_to_index in app.config['KEYS_TO_INDEX']:
             genome_name_words = genome[genome_key_to_index].lower().split()
-            group_and_location = {}
+            match_positions = {}
             for query_word in self.query_words:
                 current_location = 0
                 for nth_word in range(len(genome_name_words)):
                     if genome_name_words[nth_word][:len(query_word)] == query_word:
                         # print('******Match*******')
-                        group_and_location.setdefault('match_in_words', set()).add(nth_word + 1)
-                        group_and_location.setdefault('position_in_genome_name', {}).setdefault(query_word, []).append(current_location)
+                        match_positions.setdefault('match_in_nth_word', set()).add(nth_word + 1)
+                        match_positions.setdefault('offsets', {}).setdefault(query_word, []).append(current_location)
 
                     # +1 for white space
                     current_location = current_location + len(genome_name_words[nth_word]) + 1
 
-            if any(group_and_location):
-                group_and_location_summary.setdefault('matches', {}).update({genome_key_to_index: group_and_location})
-                group_and_location_summary['full_info'] = genome
+            if any(match_positions):
+                genome_with_matched_positions.setdefault('matches_info', {}).update({genome_key_to_index: match_positions})
+                genome_with_matched_positions['genome_info'] = genome
 
         # print(group_and_location_summary)
-        return group_and_location_summary
+        return genome_with_matched_positions
 
-    def _group_by_location(self, summary, ordered_genomes):
+    def _group_by_match_position(self, grouped_by_match_position, genome_with_matched_positions):
 
-        print(summary)
+        # print(genome_with_matched_positions)
+        group_number = min(list(set.union(*[match_info['match_in_nth_word'] for match_info in genome_with_matched_positions['matches_info'].values()])))
+        grouped_by_match_position.setdefault(group_number, []).append(genome_with_matched_positions)
 
-        final_group = min(list(set.union(*[summary['matches'][key]['match_in_words'] for key in summary['matches']])))
-
-        ordered_genomes.setdefault(final_group, []).append(summary)
-
-        return ordered_genomes
-
-        #print(final_group)
-
+        return grouped_by_match_position
 
     def _prepare_response(self, grouped_genomes):
 
-        final_response = {}
-
+        response = {}
         for group_number, genomes in grouped_genomes.items():
             for genome in genomes:
-                response = {}
-                response['genome_id'] = genome['full_info']['genome_id']
-                response['reference_genome_id'] = genome['full_info']['reference_genome_id']
-                response['common_name'] = genome['full_info']['common_name']
-                response['scientific_name'] = genome['full_info']['scientific_name']
-                response['subtype'] = genome['full_info']['subtype']
-                response['matched_substrings'] = {}
 
-                for match_key, match_info in genome['matches'].items():
-                    match_location = match_info['position_in_genome_name']
+                genome_hit = dict(
+                    genome_id=genome['genome_info']['genome_id'],
+                    reference_genome_id=genome['genome_info']['reference_genome_id'],
+                    common_name=genome['genome_info']['common_name'],
+                    scientific_name=genome['genome_info']['scientific_name'],
+                    subtype=genome['genome_info']['subtype'],
+                )
 
-                    for query, matches in match_location.items():
+                for match_in_genome_name_type, match_info in genome['matches_info'].items():
+                    for query_word, offsets in match_info['offsets'].items():
+                        for offset in offsets:
+                            matched_substring = dict(
+                                length=len(query_word),
+                                offset=offset,
+                                match=match_in_genome_name_type,
+                            )
+                            genome_hit.setdefault('matched_substrings', {}).setdefault(query_word, []).append(matched_substring)
 
-                        print(match_info)
-                        for match in matches:
-                            a = {}
-                            a['lenght'] = len(query)
-                            a['offset'] = match
-                            a['match'] = match_key
-                            # a['query_word'] = query
-                            response['matched_substrings'].setdefault(query, []).append(a)
+                response.setdefault(group_number, []).append(genome_hit)
 
-
-                #response['matched_substrings'] = genome['full_info']
-
-                final_response.setdefault(group_number, []).append(response)
-
-        return final_response
-
-
-
-
+        return response
 
 
 
