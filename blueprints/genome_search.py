d1cc4b52b630c18eae3b322dff72ade744cf4d31
Sanjay Boddu <sboddu@ebi.ac.uk>
2019-06-04 17:44:53 +0100

    Response structure update and match deduplication

--- blueprints/genome_search.py
+++ blueprints/genome_search.py
@@ -63,12 +63,14 @@ class Search(Resource):
                 genome_keys.append(set(genome_keys_of_token))
 
         # Create a set of Genome keys which are common to all the words in a user query
-        valid_genome_keys = set.intersection(*genome_keys)
+        # print(genome_keys)
+        if any(genome_keys):
+            return set.intersection(*genome_keys)
+        else:
+            return set()
 
         # print(genome_keys)
         # print(valid_genome_keys)
-
-        return valid_genome_keys
 
     def _get_genome(self, genome_key):
 
@@ -89,21 +91,36 @@ class Search(Resource):
 
     def _locate_match_positions(self, genome):
 
+
+        # print(genome)
+
         genome_with_matched_positions = {}
         # is there a match in common name? get the coordinates
         for genome_key_to_index in app.config['KEYS_TO_INDEX']:
-            genome_name_words = genome[genome_key_to_index].lower().split()
+            genome_name_words = genome[genome_key_to_index].split()
             match_positions = {}
             for query_word in self.query_words:
-                pattern = re.compile(query_word.lower())
                 current_location = 0
                 for nth_word in range(len(genome_name_words)):
+                    # print(query_word)
+                    # print(genome_name_words[nth_word])
+                    if query_word.lower() == genome_name_words[nth_word][:len(query_word)].lower():
                     # print(query_word, genome_name_words[nth_word])
-                    search = pattern.search(genome_name_words[nth_word].lower())
-                    if search:
                         # print('******Match*******')
                         match_positions.setdefault('match_in_nth_word', set()).add(nth_word + 1)
-                        match_positions.setdefault('offsets', {}).setdefault(query_word, []).append(current_location + search.start())
+
+                        # if current_location in match_positions['offsets'].keys():
+                        #     if match_positions['offsets'][current_location] < len(query_word):
+                        #         match_positions['offsets'].update({current_location: len(query_word)})
+                        # else:
+                        #     match_positions['offsets'].update({current_location: len(query_word)})
+
+                        match_positions.setdefault('offsets', {})
+                        if current_location not in match_positions['offsets'].keys() or \
+                                match_positions['offsets'][current_location] < len(query_word):
+                            match_positions['offsets'].update({current_location: len(query_word)})
+
+
 
                     # +1 for white space
                     current_location = current_location + len(genome_name_words[nth_word]) + 1
@@ -111,6 +128,8 @@ class Search(Resource):
             if any(match_positions):
                 genome_with_matched_positions.setdefault('matches_info', {}).update({genome_key_to_index: match_positions})
                 genome_with_matched_positions['genome_info'] = genome
+
+        # print(genome_with_matched_positions)
 
         # print(group_and_location_summary)
         return genome_with_matched_positions
@@ -139,14 +158,13 @@ class Search(Resource):
                 )
 
                 for match_in_genome_name_type, match_info in genome['matches_info'].items():
-                    for query_word, offsets in match_info['offsets'].items():
-                        for offset in offsets:
+                    for offset, length in match_info['offsets'].items():
                             matched_substring = dict(
-                                length=len(query_word),
+                                length=length,
                                 offset=offset,
                                 match=match_in_genome_name_type,
                             )
-                            genome_hit.setdefault('matched_substrings', {}).setdefault(query_word, []).append(matched_substring)
+                            genome_hit.setdefault('matched_substrings', []).append(matched_substring)
 
                 group.append(genome_hit)
             response.append(group)
