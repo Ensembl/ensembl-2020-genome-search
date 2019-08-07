c1e6f0f7e7a6134a802f33f77db12d009d9e8728
Kamal Dodiya <kamal@ebi.ac.uk>
2019-08-07 08:57:16 +0100

    Add blueprint for region_info

---
+++ blueprints/region_info.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, jsonify, make_response, abort
+from flask import current_app as app
+from flask_restful import Resource, Api, reqparse
+
+region_info_bp = Blueprint('region_info', __name__)
+api = Api(region_info_bp)
+
+class RegionInfo(Resource):
+    def get(self, **kwargs):
+
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('genome_id',  type=str, required=True, help="Missing genome_id param in the request.", location='args')
+        self.args = parser.parse_args()
+        genome_key = app.genome_store.check_if_genome_exists('genome_id', self.args.genome_id)
+
+        if genome_key is None:
+            return abort(400, {'error': 'Invalid Genome ID'})
+
+        ris = app.region_info_store
+
+        region_info_response = []
+        if self.args.genome_id in ris.keys():
+        	region_info_response = app.region_info_store[self.args.genome_id]
+
+#        if genome['alternative_assemblies'] is None:
+#            return make_response(jsonify([]), 200)
+
+#        alt_assemblies_response = {}
+#        for alt_assembly_genome_id in genome['alternative_assemblies']:
+#            alt_genome_key = app.genome_store.check_if_genome_exists('genome_id', alt_assembly_genome_id)#
+
+#            # Check in case there is a corrupt alternative genome id loaded into genome store from configs
+#            if alt_genome_key is not None:
+#                alt_genome = app.genome_store.get_genome(alt_genome_key)
+#                alt_assemblies_response = self._prepare_response(alt_assemblies_response, alt_genome)
+
+
+        return make_response(jsonify(region_info_response), 200)
+
+    def _prepare_response(self, alt_assemblies_response, alt_genome):
+
+        alt_assemblies_info = dict(
+            assembly_name=alt_genome['assembly_name'],
+            genome_id=alt_genome['genome_id']
+        )
+
+        alt_assemblies_response.setdefault('alternative_assemblies', []).append(alt_assemblies_info)
+
+        return alt_assemblies_response
+
+
+api.add_resource(RegionInfo, '/')
